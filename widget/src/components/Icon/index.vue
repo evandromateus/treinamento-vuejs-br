3d4a9875fffc121752328755918a167292040cbd
evandromateus <evandromateus06@gmail.com>
2021-11-25 22:11:35 -0300

    create widget button

---
+++ widget/src/components/Icon/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="name" v-bind="$props"/>
+</template>
+
+<script>
+import Loading from './Loading.vue'
+import Copy from './Copy.vue'
+import ChevronDown from './ChevronDown.vue'
+import Chat from './Chat.vue'
+
+export default {
+  components: {
+    Loading,
+    Copy,
+    Chat,
+    ChevronDown
+  },
+  props: {
+    name: { type: String, required: true }
+  }
+}
+</script>
--- widget/src/views/Playground/index.vue
+++ widget/src/views/Playground/index.vue
@@ -21,7 +21,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Widget from '../Widget'
+import Widget from '../Widget/index.vue'
 
 export default defineComponent({
   components: { Widget }
